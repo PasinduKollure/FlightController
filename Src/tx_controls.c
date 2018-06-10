97f6708624a0502038ade49b0da514bcd81da62d
pasindu kollure <pasindu.kollure@gmail.com>
2018-06-10 19:52:19 -0400

    Added new tx_controls files

---
+++ Src/tx_controls.c
@@ -0,0 +1,27 @@
+/*
+ * tx_decoder.c
+ *
+ *  Created on: June 9, 2018
+ *      Author: Pasindu
+ */
+
+#include "stm32f4xx_hal.h"
+#include "tx_controls.h"
+#include "master_configuration.h"
+
+TxCtrlData turnigy;
+
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart){
+	/*
+	ctrlData[0]: Header
+	ctrlData[1]: Yaw
+	ctrlData[2]: Pitch
+	ctrlData[3]: Throttle
+	ctrlData[4]: Roll
+	*/
+	
+	UNUSED(huart);
+	
+	for(int i=0; i<WORD_COUNT; i++)
+		turnigy.ctrlData[i] = turnigy.rawData[i*2+1] << 8 | turnigy.rawData[i*2];
+}
